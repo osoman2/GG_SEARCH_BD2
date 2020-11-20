5bddc79ed5072d795f9ff7d9426f689d538f94ec
THEFLILUX <jorge.nicho@utec.edu.pe>
2020-11-20 18:50:49 -0500

    fix

--- searchProject/server.js
+++ searchProject/server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-//Getting out POSTS routes
 const routes = require('./server/routes/posts');
 
-//using middleware
 app.use(express.static(path.join(__dirname, 'dist/searchProject')));
 app.use('/routes', routes);
 
-//Catch all other routes request and return it to the index
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'dist/searchProject/index.html'))
 });
--- searchProject/server/routes/posts.js
+++ searchProject/server/routes/posts.js
@@ -4,20 +4,9 @@ const router = express.Router();
 
 const axios = require('axios');
 
-const PostAPI = 'https://jsonplaceholder.typicode.com';
-
 router.get("/posts", function(req, res){
     const data = JSON.parse(fs.readFileSync('db.json'));
     res.json(data);
 })
-
-/*router.get('/posts', (req, res)=>{
-    axios.get(`${PostAPI}/posts`).then(posts=>{
-        //console.log(posts.data);
-        res.status(200).json(posts.data);
-    }).catch(error =>{
-        res.status(500).send(error);
-    })
-});*/
 
 module.exports = router;
